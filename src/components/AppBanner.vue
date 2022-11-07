0b697e58c514fce355c165b422534ef9020ae742
reistence <gvlamberti@gmail.com>
2022-11-07 15:58:41 +0100

    AppTitle and AppBanner implemented

---
+++ src/components/AppBanner.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  data() {
+    return {
+      rndImg: "https://picsum.photos/800/500",
+      myImg: "myimg.jpg",
+    };
+  },
+  methods: {
+    getImagePath: function () {
+      return new URL(`../assets/${this.myImg}`, import.meta.url).href;
+    },
+  },
+};
+</script>
+<template>
+  <div>
+    <img :src="rndImg" alt="" />
+    <img :src="getImagePath()" alt="" />
+  </div>
+</template>
+<style>
+div {
+  width: 100%;
+  height: 500px;
+  margin: 0 auto;
+  border-radius: 10px;
+  overflow: hidden;
+}
+img {
+  width: 50%;
+  object-fit: cover;
+  height: 100%;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "My first Vite-app!",
+    };
+  },
+};
+</script>
+<template>
+  <h1>{{ title }}</h1>
+</template>
+<style scoped>
+h1 {
+  font-size: 3rem;
+  color: red;
+  text-align: center;
+}
+</style>
